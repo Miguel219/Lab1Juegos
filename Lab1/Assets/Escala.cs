5e1b9b0467558944737feea910444ff66ae91da9
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2019-03-24 20:31:55 -0600

    Cambios

---
+++ Lab1/Assets/Escala.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escala : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((Input.GetKey(KeyCode.K))&& (transform.localScale.x != 4)) {
+            transform.localScale += new Vector3(1, 1, 1);
+        }else if ((Input.GetKey(KeyCode.L))&& (transform.localScale.x != 1)) {
+            transform.localScale += new Vector3(-1, -1, -1);
+        }
+    }
+}
---
+++ Lab1/Assets/Oscilar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscilar : MonoBehaviour {
+
+    bool movingUp = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (movingUp) {
+            transform.Translate(0f, 1f * Time.deltaTime, 0f);
+            if (transform.position.y >= 3)
+            {
+                movingUp = false;
+            }
+        }
+        else {
+            transform.Translate(0f, -1f * Time.deltaTime, 0f);
+            if (transform.position.y <= 0.5)
+            {
+                movingUp = true;
+            }
+        }
+	}
+}
